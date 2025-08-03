919945252eaedccd6cf3ebdaa0ce2c7406879583
Isaac <icw5055@psu.edu>
2025-08-03 17:02:36 +1200

    Test

--- Small magic.jl
+++ Small magic.jl
@@ -4,23 +4,8 @@
 using Markdown
 using InteractiveUtils
 
-# ╔═╡ e06835d0-7017-11f0-0d27-5de33db62d77
-begin
-    using Pkg
-    Pkg.add("Downloads")
-    Pkg.add("DelimitedFiles")
-end
-
-
-# ╔═╡ a1f82f16-87a4-4318-ac60-23afa2b31dc9
-begin
-	using DelimitedFiles
-	
-	# Example: read conversion factors
-	kcmb2ysz = readdlm("SZ-Project")
-	
-end
+# ╔═╡ 4025de0e-cfa5-4586-b311-6c2272d5173c
+hello there
 
 # ╔═╡ Cell order:
-# ╠═e06835d0-7017-11f0-0d27-5de33db62d77
-# ╠═a1f82f16-87a4-4318-ac60-23afa2b31dc9
+# ╠═4025de0e-cfa5-4586-b311-6c2272d5173c
