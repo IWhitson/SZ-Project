e54f908b76c11d2e78d277008c4cce5e520ae455
Isaac <icw5055@psu.edu>
2025-08-29 14:00:32 +1200

    Working

--- Small magic.jl
+++ Small magic.jl
@@ -8,10 +8,7 @@ using InteractiveUtils
 using LsqFit, CSV, DataFrames, PlutoUI, Plots, PlutoTeachingTools, NPZ, LaTeXStrings, Statistics, LinearAlgebra, DelimitedFiles, Optim, Distributions, Printf, UltraNest, Conda, PyCall, DynamicHMC, LogDensityProblems, Random, StatsPlots
 
 # ╔═╡ 5e82a784-3fbd-4723-aab3-a00e4e77d6b3
-begin
-	include("SZ-Defs.jl")
-	include("SZ-Terms.jl")
-end
+include("SZ-Defs.jl")
 
 # ╔═╡ 456f552a-99d8-48e2-93cc-153501bf00fc
 begin
@@ -35,6 +32,19 @@ end
 
 # ╔═╡ e3bd1e42-0ab9-48da-a858-a824734122a0
 begin
+	# --- Load conversion tables ---
+	poly_pars = load_numeric_matrix("YrSZ2KCMB_polyfits.txt")
+	Tconv = load_numeric_matrix("KCMB2MJysr.txt")[:, 1]
+	yconv = load_numeric_matrix("KCMB2YSZ.txt")[:, 1]
+		
+	# --- Load band definitions and error bars ---
+	bands = load_numeric_matrix("sensitivity_calculations.txt")
+	band_inds = bands[:, 1]
+	band_errs = bands[:, 6]
+	nband = length(band_inds)
+	y = 1e-4
+	Te = 10.0
+
 	SZsig = zeros(nband)
 	nrSZsig = zeros(nband)
 	for i in 1:nband
@@ -48,6 +58,9 @@ end
 
 # ╔═╡ a352f09c-e979-47e7-b6e2-52e23487eafd
 begin
+	y_obs = 1.1e-4
+	observed = [1.0 / yconv[i] * Tconv[i] * y_obs for i in 1:length(band_inds)]
+
 	nr_expected, nr_errors = load_expected_and_error("Te_10.0_tSZ_signal.txt")
 	rel_expected, rel_errors = load_expected_and_error("Te_10.0_rSZ_signal.txt")
 
